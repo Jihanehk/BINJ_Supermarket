a6b7b0bc184bb764c02977bf217d1835b520c1b3
Jihane <Jihane@DESKTOP-FVSKSQA>
2018-03-09 09:12:59 +0100

    test commit

---
+++ Projet_Final/GestionGrandeSurface/GestionGrandeSurface-ejb/src/java/Structure/Requete.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Structure;
+
+/**
+ *
+ * @author Nawar
+ */
+public abstract class Requete {
+    public static String getRoles = "SELECT r FROM ROLE AS r WHERE 1=1";
+    public static String getEmployes = "SELECT e FROM Employes AS e WHERE 1=1";
+}
---
+++ Projet_Final/GestionGrandeSurface/GestionGrandeSurface-war/web/JSP_Pages/MesJavascript.js
@@ -0,0 +1,69 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+/////////////////////////////////////////////////////
+function RechercherEmployeParNom() {     
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+
+function addRowHandlersDirectionGeneral() {
+var table = document.getElementById("myTable");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        var currentRow = table.rows[i];
+        var createClickHandler = 
+            function(row) 
+            {
+                return function() { 
+                                        var cell = row.getElementsByTagName("td")[0];
+                                        var id = cell.innerHTML;
+                                        //alert("id:" + id);
+                                        if (confirm('Vous etes sur de vouloir faire employe '+id+' un direction general?')) {
+                                                document.getElementsByName("employe").value = id;
+                                         } else {
+                                                // Do nothing!
+                                         }
+                                       
+                                 };
+            };
+
+        currentRow.onclick = createClickHandler(currentRow);
+    }
+  // alert("koko");
+}
